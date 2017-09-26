4133c747a327528f15a06383c54249b0c1d77182
Sindre Svendby <sinsvend@gmail.com>
2017-09-26 22:02:27 +0200

    linter fixes

--- helpers.js
+++ helpers.js
@@ -1,10 +1,10 @@
 /* @flow */
-import debug from 'debug';
+import debug from "debug";
 
-export const log = debug('cupassist-update-player-api');
-export const debugError = debug('error');
-export const debugOk = debug('ok');
-export const debugComplete = debug('Complete');
+export const log = debug("cupassist-update-player-api");
+export const debugError = debug("error");
+export const debugOk = debug("ok");
+export const debugComplete = debug("Complete");
 
 export function addField(object: Object, newObject: Object) {
   return Object.assign({}, object, newObject);
--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 /* @flow */
 
-import Rx from 'rxjs/Rx';
-import { map, mergeAll, flatMap } from 'rxjs';
-import { mapTo } from 'rxjs/operator/mapTo';
-import { concatMap } from 'rxjs/operator/concatMap';
+import Rx from "rxjs/Rx";
+import { map, mergeAll, flatMap } from "rxjs";
+import { mapTo } from "rxjs/operator/mapTo";
+import { concatMap } from "rxjs/operator/concatMap";
 
-import lodash from 'lodash'
-import sendToApi from './player-api'
-import getCupassistData from './cupassist'
-import { debugOk, debugError, debugComplete, log } from './helpers'
+import lodash from "lodash";
+import sendToApi from "./player-api";
+import getCupassistData from "./cupassist";
+import { debugOk, debugError, debugComplete, log } from "./helpers";
 
-const players = []
+const players = [];
 
 const delayWithSelector = Rx.Observable.prototype.delayWithSelector;
 
 const Promise = require("bluebird");
 
-if(typeof process.env.CUPASSIST_API === 'undefined' || process.env.CUPASSIST_API === null) {
-  throw new Error('Please set CUPASSIST_API as a env.var');
+if (
+  typeof process.env.CUPASSIST_API === "undefined" ||
+  process.env.CUPASSIST_API === null
+) {
+  throw new Error("Please set CUPASSIST_API as a env.var");
 }
 
 const CUPASSIST_API = `${process.env.CUPASSIST_API}/ranking`;
 
-const YEARS= [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016];
+const YEARS = [2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016];
 
-const requestData$ = Rx.Observable.interval(200)
-      .take(YEARS.length)
-      .map((i) => YEARS[i])
-      .map(populateYearsUrls)
-      .concatMap(populateGenderUrls)
+const requestData$ = Rx.Observable
+  .interval(200)
+  .take(YEARS.length)
+  .map(i => YEARS[i])
+  .map(populateYearsUrls)
+  .concatMap(populateGenderUrls);
 
 const players$ = requestData$
   .concatMap(getCupassistData)
@@ -36,39 +40,43 @@ const players$ = requestData$
   .concatMap(extractPlayers)
   .filter(object => object.name !== undefined)
   .groupBy(getPlayerId)
-  .flatMap(createDBFormat)
+  .flatMap(createDBFormat);
 
-players$
-  .subscribe(savePlayers, debugError, startProcessing);
-      // .subscribe(handlePlayers, debugError, debugComplete);
-
+players$.subscribe(savePlayers, debugError, startProcessing);
+// .subscribe(handlePlayers, debugError, debugComplete);
 
 function savePlayers(player) {
   players.push(player);
 }
 
 function startProcessing() {
-  log('startProcessing')
+  log("startProcessing");
   Rx.Observable
     .interval(200)
     .take(players.length)
     .map(i => players[i])
-    .subscribe(sendToApi, debugError, debugComplete)
+    .subscribe(sendToApi, debugError, debugComplete);
 }
 
 function createDBFormat(groupedObservable) {
-  return groupedObservable.reduce((aggObject, playerYear) => {
-      const playerRankingYear = { [`rankingPoints_${playerYear.year}`]: playerYear.points}
+  return groupedObservable
+    .reduce((aggObject, playerYear) => {
+      const playerRankingYear = {
+        [`rankingPoints_${playerYear.year}`]: playerYear.points
+      };
       return Object.assign({}, aggObject, playerYear, playerRankingYear);
     }, {})
     .map(player => {
-      return Object.assign({
-        firstName: player.name.split(' ').slice(0, -1).join(" ").trim(),
-        lastName: player.name.split(' ').slice(-1).join(" ").trim(),
-        name: player.name,
-        externalId: player.cupassistId,
-      },lodash.omit(player, ['year', 'points', 'name', 'cupassistId', 'url']))
-    })
+      return Object.assign(
+        {
+          firstName: player.name.split(" ").slice(0, -1).join(" ").trim(),
+          lastName: player.name.split(" ").slice(-1).join(" ").trim(),
+          name: player.name,
+          externalId: player.cupassistId
+        },
+        lodash.omit(player, ["year", "points", "name", "cupassistId", "url"])
+      );
+    });
 }
 
 function getPlayerId(object) {
@@ -77,20 +85,20 @@ function getPlayerId(object) {
 
 function extractPlayers(response) {
   return response.data.map(player => {
-    return Object.assign({}, lodash.omit(response, 'data'), player);
-  })
+    return Object.assign({}, lodash.omit(response, "data"), player);
+  });
 }
 
 function populateYearsUrls(year) {
   return {
     year: year,
     url: `${CUPASSIST_API}/${year}`
-  }
+  };
 }
 
 function populateGenderUrls(object) {
   return [
-    Object.assign({}, object, { url:`${object.url}/K`, gender: 'K' }),
-    Object.assign({}, object, { url:`${object.url}/M`, gender: 'M' }),
+    Object.assign({}, object, { url: `${object.url}/K`, gender: "K" }),
+    Object.assign({}, object, { url: `${object.url}/M`, gender: "M" })
   ];
 }
